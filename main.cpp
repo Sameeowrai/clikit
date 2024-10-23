d34e61894cc11a27630d7dc9d529473250194979
Sameeowrai <sameeowrai@gmail.com>
2024-10-23 05:21:20 +0330

    Create menu item structure and make menu class more dynamic

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <unistd.h>
 #include <termios.h>
+#include <variant>
+
 
 using namespace std;
 unsigned int microsecond = 1000000;
@@ -35,14 +37,22 @@ void print(string text, float time, string colorCode = defaultColor) {
     cout << endl;
 }
 
+template <typename T>
+struct MenuItem{
+    string text;
+    string color;
+    T value;
+};
+
+template <typename T>
 class Menu {
-    vector<string> m_menuItems;
+    vector<MenuItem<T>> m_menuItems;
     string m_header;
     int selectedItem = 0;
     float displaySpeed;
 
 public:
-    Menu(string header, vector<string> menuItems, float speed = 0.01) : m_menuItems(menuItems), m_header(header, displaySpeed = speed ) {}
+    Menu(string header, vector<MenuItem<T>> menuItems, float speed = 0.01) : m_menuItems(menuItems), m_header(header), displaySpeed(speed)  {}
 
     void Display(float time) {
         #ifdef _WIN32
@@ -53,14 +63,14 @@ public:
         print(m_header, time);
         for (int i = 0; i < m_menuItems.size(); ++i) {
             if (i == selectedItem) {
-                print("-> " + m_menuItems[i], time, "\033[1;32m");
+                print("-> " + m_menuItems[i].text, time, "\033[1;32m");
             } else {
-                print("  " + m_menuItems[i], time);
+                print("  " + m_menuItems[i].text, time,m_menuItems[i].color);
             }
         }
     }
 
-    int Get() {
+    T Get() {
         enableRawMode();
         char c;
         Display(displaySpeed);
@@ -74,22 +84,27 @@ public:
                 }
             } else if (c == ENTER) {
                 disableRawMode();
-                return selectedItem;
+                return m_menuItems[selectedItem].value;
             }
             Display(0);
         }
     }
 
-    void Insert(string text){
-        m_menuItems.push_back(text);
+    void Insert(MenuItem<T> item){
+        m_menuItems.push_back(item);
     }
+
 };
 
 
 int main(){
     print("Hello world",0.1);
     while(1){
-        Menu menu("Menu", {"Home","Contact Us","Exit"},0.05);
+        Menu<int> menu("Menu", {
+            {"Home","\e[1;35m", 0},
+            {"Contact Us","\e[1;34m", 1},
+            {"Exit","\e[1;31m", 2}
+        },0.05);
         int res = menu.Get();
         switch (res)
         {
@@ -100,7 +115,10 @@ int main(){
             print("-> My github: https://github.com/Sameeowrai\n-> This project repo: https://github.com/Sameeowrai/clikit",0.1,"\e[1;31m");
             break;
         case 2:{
-            Menu exitMenu("Are you sure?", {"yes","no"});
+            Menu<int> exitMenu("Are you sure?", {
+                {"yes", "\e[0;37m", 0},
+                {"no", "\e[0;37m", 1}
+            });
             if(exitMenu.Get() == 0){
                 exit(0);
             }
@@ -108,7 +126,5 @@ int main(){
         default:
             break;
         }
-        cin.ignore();
-        usleep(microsecond);
     }
 }
